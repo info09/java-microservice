e76bb57bd41f3dd7729125cef4d2d6c9f736230c
huytq@ics-p.vn <huytq@ics-p.vn>
2024-09-12 14:58:48 +0700

    add page response

---
+++ post-service/src/main/java/com/learning/post_service/dto/PageResponse.java
@@ -0,0 +1,22 @@
+package com.learning.post_service.dto;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Collections;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PageResponse<T> {
+    int currentPage;
+    int totalPage;
+    int pageSize;
+    long totalElement;
+
+    @Builder.Default
+    private List<T> data = Collections.emptyList();
+}
