23869b03a87ca469f9d3809e83ff30c554790a0f
huytq@ics-p.vn <huytq@ics-p.vn>
2024-09-13 10:14:30 +0700

    update front end

--- web-app/src/configurations/configuration.js
+++ web-app/src/configurations/configuration.js
@@ -5,5 +5,5 @@ export const CONFIG = {
 export const API = {
   LOGIN: "/identity/auth/login",
   MY_INFO: "/profile/users/my-profile",
-  PROFILE: "/profile/users/userId",
+  MY_POST: "/post/my-post",
 };
---
+++ web-app/src/services/postService.js
@@ -0,0 +1,15 @@
+import { API } from "../configurations/configuration";
+import httpClient from "../configurations/httpClient";
+import { getToken } from "./localStorageService";
+
+export const getMyPosts = async (page) => {
+  return await httpClient.get(API.MY_POST, {
+    headers: {
+      Authorization: `Bearer ${getToken()}`,
+    },
+    params: {
+      page: page,
+      size: 10,
+    },
+  });
+};
